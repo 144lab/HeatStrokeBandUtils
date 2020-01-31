8470f128c55e3fcb78192a52a37aa009f9eb6c2e
Irieda Noboru <irieda@144lab.com>
2020-01-31 13:01:48 +0900

    3mode support

--- docs/app.js
+++ docs/app.js
@@ -1,115 +1,128 @@
 const serviceUuid = "30c4d481-ea34-457b-8d54-5efc625241f7";
 const writeUUID = "e9062e71-9e62-4bc6-b0d3-35cdcd9b027b";
-const rawNotifyUUID = "5008e0bd-3581-4d4c-a6d8-c257a369e189";
-const envNotifyUUID = "b84ea3e8-b237-4b95-a394-6911180b7638";
 
-var server, write, notify;
+const rawNotifyUUID = "5008e0bd-3581-4d4c-a6d8-c257a369e189";
+const rriNotifyUUID = "b84ea3e8-b237-4b95-a394-6911180b7638";
+const envNotifyUUID = "62fbd229-6edd-4d1a-b554-5c4e1bb29169";
+
+const config = {
+  Waveform: {
+    uuid: rawNotifyUUID,
+    write: new Uint8Array([0xfd]), /// RAW-MODE
+    dataRate: 32,
+    fname: "waveform.csv",
+    makeLines: parseWave
+  },
+  RRI: {
+    uuid: rriNotifyUUID,
+    write: new Uint8Array([0xfc]), /// RRI-MODE
+    dataRate: 1,
+    fname: "rri.csv",
+    makeLines: parseRRI
+  },
+  Normal: {
+    uuid: envNotifyUUID,
+    write: new Uint8Array([0x01]), /// NORMAL-MODE
+    dataRate: 1,
+    fname: "normal.csv",
+    makeLines: parseEnv
+  }
+};
+var close = undefined;
 var store = [];
 
-document.getElementById("dump-stop").disabled = true;
-document.getElementById("dump-stop").addEventListener("click", dumpStop);
-document.getElementById("dump-start").addEventListener("click", dumpStart);
-document.getElementById("test-stop").disabled = true;
-document.getElementById("test-stop").addEventListener("click", testStop);
-document.getElementById("test-start").addEventListener("click", testStart);
-
-async function dumpStart(ev) {
-  store = [];
+async function collectStart(ev) {
+  var mode = document.getElementById("type").value;
+  console.log("start: " + mode);
   ev.preventDefault();
-  console.log("dump-start");
+  const settings = config[mode];
+  store = [];
   const device = await navigator.bluetooth.requestDevice({
     filters: [{ services: [serviceUuid] }]
   });
-  device.addEventListener("gattserverdisconnected", dumpStop);
+  device.addEventListener("gattserverdisconnected", collectStop);
   console.log("Connecting to GATT Server...");
-  server = await device.gatt.connect();
+  var server = await device.gatt.connect();
   console.log("Getting Service...");
   const service = await server.getPrimaryService(serviceUuid);
   console.log("Getting Characteristic...");
   // console.log(await service.getCharacteristics());
-  write = await service.getCharacteristic(writeUUID);
-  console.log(write);
-  notify = await service.getCharacteristic(rawNotifyUUID);
-  console.log(notify);
+  var write = await service.getCharacteristic(writeUUID);
+  var notify = await service.getCharacteristic(settings.uuid);
   notify.addEventListener("characteristicvaluechanged", async event => {
     const value = event.target.value;
     store.push(value.buffer);
-    document.getElementById("dump-count").innerText = store.length * 32;
+    document.getElementById("count").value = store.length * settings.dataRate;
   });
   await notify.startNotifications();
-  write.writeValue(new Uint8Array([0xfd])); // ENTER_RAW_MODE
-  document.getElementById("dump-start").disabled = true;
-  document.getElementById("dump-stop").disabled = false;
+  close = async () => {
+    if (server.connected) {
+      await notify.stopNotifications();
+      if (server != undefined) {
+        await server.disconnect();
+        server = undefined;
+      }
+    }
+  };
+  write.writeValue(settings.write); // ENTER_RAW_MODE
+
+  document.getElementById("start").disabled = true;
+  document.getElementById("stop").disabled = false;
+  document.getElementById("download").classList.add("disabled");
   return false;
 }
 
-async function dumpStop(ev) {
-  console.log("dump-stop");
+async function collectStop(ev) {
+  var mode = document.getElementById("type").value;
+  console.log("stop: " + mode);
   ev.preventDefault();
-  document.getElementById("dump-stop").disabled = true;
-  document.getElementById("dump-start").disabled = false;
-  if (server.connected) {
-    await notify.stopNotifications();
-    if (server != undefined) {
-      await server.disconnect();
-      server = undefined;
-    }
-  }
+  const settings = config[mode];
+
+  document.getElementById("stop").disabled = true;
+  document.getElementById("start").disabled = false;
+  if (close) await close();
+  var blob = new Blob(settings.makeLines(store), { type: "text/csv" });
+  document.getElementById("download").download = settings.fname;
+  document.getElementById("download").href = window.URL.createObjectURL(blob);
+  document.getElementById("download").classList.remove("disabled");
+  return false;
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("stop").disabled = true;
+  document.getElementById("stop").addEventListener("click", collectStop);
+  document.getElementById("start").addEventListener("click", collectStart);
+});
+
+function parseWave(s) {
   var lines = [];
-  store.forEach(v => {
+  s.forEach(v => {
     var a = new Uint16Array(v);
     a.forEach(val => {
       lines.push(val + "\n");
     });
   });
-  var blob = new Blob(lines, { type: "text/plain" });
-  document.getElementById("dump-download").href = window.URL.createObjectURL(
-    blob
-  );
-  return false;
+  return lines;
 }
 
-async function testStart(ev) {
-  store = [];
-  ev.preventDefault();
-  console.log("test-start");
-  const device = await navigator.bluetooth.requestDevice({
-    filters: [{ services: [serviceUuid] }]
+function parseRRI(s) {
+  var lines = [];
+  s.forEach(v => {
+    var a = new Uint16Array(v);
+    a.forEach(val => {
+      lines.push(val + "\n");
+    });
   });
-  device.addEventListener("gattserverdisconnected", dumpStop);
-  console.log("Connecting to GATT Server...");
-  server = await device.gatt.connect();
-  console.log("Getting Service...");
-  const service = await server.getPrimaryService(serviceUuid);
-  console.log("Getting Characteristic...");
-  // console.log(await service.getCharacteristics());
-  write = await service.getCharacteristic(writeUUID);
-  console.log(write);
-  notify = await service.getCharacteristic(envNotifyUUID);
-  console.log(notify);
-  notify.addEventListener("characteristicvaluechanged", async event => {
-    const value = event.target.value;
-    store.push(value.buffer);
-    document.getElementById("test-count").innerText = store.length;
-  });
-  await notify.startNotifications();
-  write.writeValue(new Uint8Array([0xfd])); // ENTER_RAW_MODE
-  document.getElementById("test-start").disabled = true;
-  document.getElementById("test-stop").disabled = false;
-  return false;
+  return lines;
 }
 
-async function testStop(ev) {
-  console.log("test-stop");
-  ev.preventDefault();
-  document.getElementById("test-stop").disabled = true;
-  document.getElementById("test-start").disabled = false;
-  if (server.connected) {
-    await notify.stopNotifications();
-    if (server != undefined) {
-      await server.disconnect();
-      server = undefined;
-    }
-  }
-  return false;
+function parseEnv(s) {
+  var lines = [];
+  s.forEach(v => {
+    var a = new Uint16Array(v);
+    a.forEach(val => {
+      lines.push(val + "\n");
+    });
+  });
+  return lines;
 }
