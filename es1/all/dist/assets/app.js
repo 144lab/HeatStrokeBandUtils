c3a61dcc4da1d78ba1fe56c7f676ac632dabcf19
Irieda Noboru <irieda@144lab.com>
2020-07-20 17:37:02 +0900

    fix box

--- es1/all/dist/assets/app.js
+++ es1/all/dist/assets/app.js
@@ -1,7 +1,8 @@
-const f = 1 / 20;
-const hr = document.querySelector(".heartrate");
 const startDummyHeartRate = (ms) => {
+  const f = 1 / 20;
+  const hr = document.querySelector(".heartrate");
   const t = ms / 1000.0;
   hr.style.animationDuration = 0.3 + 0.1 * Math.sin(2 * Math.PI * f * t);
   requestAnimationFrame(startDummyHeartRate);
 };
+window.startDummyHeartRate = startDummyHeartRate;
